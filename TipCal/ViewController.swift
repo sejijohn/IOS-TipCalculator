5edd1384903afbf9de0cadf15aff0fd93cb8944d
Seji John <sejijohn@Sejis-MacBook-Pro.local>
2017-03-03 14:51:27 -0800

    First Commit

--- TipCal/ViewController.swift
+++ TipCal/ViewController.swift
@@ -9,7 +9,12 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var billText: UITextField!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    @IBOutlet weak var divideByText: UITextField!
+    @IBOutlet weak var perPersonLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +25,31 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func onTap(_ sender: AnyObject) {
+        view.endEditing(true)
+    }
+    
+    
+    
+    @IBAction func calculateTip(_ sender: AnyObject) {
+        let tipPercentage = [0.09,0.12,0.15,0.18,0.20]
+        let bill = Double(billText.text!) ?? 0
+        let divideBy = Double(divideByText.text!) ?? 1
+        let tip = bill * tipPercentage[tipControl.selectedSegmentIndex]
+        let total = bill + tip
+        if(divideBy != 0){
+           let perPerson = total/divideBy
+            perPersonLabel.text = String(format: "$%.2f", perPerson)
+        } else{
+            perPersonLabel.text = "Error"
+        }
+        
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+        
+        
+        
+    }
 
 }
 
